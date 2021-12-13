d9a618c079abcfec58495dc5c8626a60da0051a3
Marcelo Hernandez <marcelohdez41@gmail.com>
2021-12-13 16:09:41 -0500

    removed padding from macOS buttons to better fit in with other apps

--- src/main/java/me/soggysandwich/define4me/Define4Me.java
+++ src/main/java/me/soggysandwich/define4me/Define4Me.java
@@ -10,6 +10,7 @@ import java.awt.event.KeyListener;
 public class Define4Me {
 
     public static final String VERSION = "1.0";
+    private static boolean isMacOS;
 
     public static void main(String[] args) {
         // Try to get the system's look and feel and set to it
@@ -21,8 +22,10 @@ public class Define4Me {
             } catch (Exception i) { i.printStackTrace(); } // Print error if no look and feel is available
         }
         // If we are on macOS, enable use of the menu bar at the top of the screen (if enabled in settings)
-        if (System.getProperty("os.name").equals("Mac OS X") && Settings.prefersMacMenuBar())
-            System.setProperty("apple.laf.useScreenMenuBar", "true");
+        if (System.getProperty("os.name").equals("Mac OS X")) {
+            isMacOS = true;
+            if (Settings.prefersMacMenuBar()) System.setProperty("apple.laf.useScreenMenuBar", "true");
+        }
 
         new AppWindow(); // Open app window
     }
@@ -37,7 +40,7 @@ public class Define4Me {
         for (AbstractButton b : buttons) {
             if (kl != null) b.addKeyListener(kl);
             b.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-            if (b instanceof JButton) b.setMargin(new Insets(6, 12, 6, 12));
+            if (!isMacOS && b instanceof JButton) b.setMargin(new Insets(6, 12, 6, 12));
         }
     }
 
