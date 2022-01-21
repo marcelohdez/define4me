324ebe13fee7c0854a00481a01b5e9a25bc1550f
swiftsatchel <marcelohdez41@gmail.com>
2022-01-21 17:42:12 -0500

    cleanup WordParser

--- src/main/java/me/soggysandwich/define4me/util/WordParser.java
+++ src/main/java/me/soggysandwich/define4me/util/WordParser.java
@@ -3,39 +3,31 @@ package me.soggysandwich.define4me.util;
 public final class WordParser {
 
     /**
-     * Parses the given string for its word
+     * Trims the given string and removes any extra spacing between words,
+     * then returns the remaining letters, spaces, and hyphens.
      *
      * @param string Given string, preferably a single line
      * @return The parsed letters, hyphens, or spaces
      */
     public static String parseString(String string) {
         StringBuilder wordBuilder = new StringBuilder();
-        // Find starting letter to not keep any starting space, ex: "  hi" -> "hi"
-        int firstLetter = 0;
-        for (int i = 0; i < string.length(); i++)
-            if (Character.isLetter(string.charAt(i))) {
-                firstLetter = i;
-                break;
-            }
+        string = string.trim();
 
         // Check through every character
-        int lastLetter = firstLetter;
-        for (int i = firstLetter; i < string.length(); i++) {
-            if (string.startsWith(" -", i)) break; // For lists with hyphens after words
+        for (int i = 0; i < string.length(); i++) {
+            if (string.startsWith(" -", i)) break; // For lists with hyphens at the end of words
             char c = string.charAt(i); // Save current char
-            // If character is a space and there's no letter afterwards continue, else add it.
+
+            // If character is a space and there's no letter afterwards go on to next char, else add it.
             if (Character.isSpaceChar(c)) {
                 if (i + 1 < string.length()) {
                     if (!Character.isLetter(string.charAt(i + 1))) continue;
                     wordBuilder.append(c);
                 }
-            } else if (Character.isLetter(c)) { // Else if it's a letter add it and set lastLetter to next index
+            } else if (Character.isLetter(c) || c == '-') { // Else if it's a letter add it
                 wordBuilder.append(c);
-                lastLetter = i + 1;
-            } else if (c == '-') wordBuilder.append(c); // Else if it's a hyphen just add it.
+            }
         }
-        // Remove stuff after last letter:
-        if (wordBuilder.length() > lastLetter) wordBuilder.delete(lastLetter, wordBuilder.length());
 
         return wordBuilder.toString();
     }
