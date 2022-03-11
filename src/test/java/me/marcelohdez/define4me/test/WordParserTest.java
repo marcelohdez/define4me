0fd3c12ef61053e8cde107be4c293059701be4a3
Marcelo Hernandez <marcelohdez41@gmail.com>
2022-03-11 13:41:04 -0500

    added WordParser tests

---
+++ src/test/java/me/marcelohdez/define4me/test/WordParserTest.java
@@ -0,0 +1,33 @@
+package me.marcelohdez.define4me.test;
+
+import me.marcelohdez.define4me.util.WordParser;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordParserTest {
+    @Test
+    public void removeExtraSpaces() {
+        assertEquals("One two three", WordParser.parseString("One    two  three"));
+    }
+
+    @Test
+    public void removeExtraHyphens() {
+        assertEquals("One-two-three", WordParser.parseString("One---two--three"));
+    }
+
+    @Test
+    public void capitalizeFirstLetter() {
+        assertEquals("Word", WordParser.parseString("word"));
+    }
+
+    @Test
+    public void removeEverythingBeforeFirstLetter() {
+        assertEquals("Hello", WordParser.parseString("?//-  hello"));
+    }
+
+    @Test
+    public void removeEverythingAfterLastLetter() {
+        assertEquals("Hello", WordParser.parseString("hello--!"));
+    }
+}
